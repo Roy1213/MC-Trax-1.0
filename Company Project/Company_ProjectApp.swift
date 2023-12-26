459f607bae6ff8915cb004011e26376427078e27
Roy Alameh <royalameh1213@gmail.com>
2023-12-25 20:51:13 -0600

    Initial Commit

---
+++ Company Project/Company_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Company_ProjectApp.swift
+//  Company Project
+//
+//  Created by Roy Alameh on 12/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Company_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
